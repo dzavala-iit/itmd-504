14ec6a201b3005e0c01929868ce9ba616ffca259
dzavala-iit <164047261+dzavala-iit@users.noreply.github.com>
2025-03-27 18:21:45 +0000

    added line 5 oin app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import emp_data from './emp_data';
 
 
 function App() {
