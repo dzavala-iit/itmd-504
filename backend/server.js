6fc4cd748a3adc48efe529ffd8946e0aaa5777b6
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-27 17:39:34 +0000

    adding information to the backend of the db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql")
+
 const app = express();
+
+app.use(cors());
+
+mysql.createConnection({
+    host: "database-1.c3agy6ykydbf.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "TfgCbs|[Jl9k|6G_!vX2k)MG(ae>",
+    database: "itmd-504"
+})
 
 app.listen(8081, () => {
     console.log("Server is running on port 8081 and listening");
