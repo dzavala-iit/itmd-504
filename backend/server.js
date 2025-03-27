ad0c3b947089b2018f7a288f00bc630858cf44f7
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-27 21:34:31 +0000

    no major changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql")
+const mysql = require("mysql");
 
 const app = express();
 
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
 })
 
 app.get("/", (req, res) => {
-    const sql = "SELECT * FROM emp_data";
+    const sql = "SELECT * FROM Employee";
     db.query(sql, (err, data) => {
         if (err) return res.json("Error");
         return res.json(data);
