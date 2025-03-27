99e32293b34a954697013d971ac45692b5e4ce84
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-27 19:55:38 +0000

    adjjusting changes to front end

--- frontend/src/emp_data.js
+++ frontend/src/emp_data.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import axios from 'axios'
 
 function emp_data() {
+
+        useEffect(()=> {
+            axios.get('http://172.31.4.53:8081/')
+            ;then(res => console.log(res))
+            .catch(err => console.log(err));
+        },[])   
+
     return (
-        <div>emp_data</div>
+        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+            <div className='w-50 bg-white rounded'>
+                <button className='btn btn-success'>Add +</button>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
