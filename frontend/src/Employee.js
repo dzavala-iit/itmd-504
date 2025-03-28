d299d85dcfe6db0111536dafc0b53d2d9e1ae300
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-28 17:14:08 +0000

    adding Link to add data to db

--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 
 function Employee() {
@@ -12,7 +13,7 @@ function Employee() {
     return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-5'>
-                <button className='btn btn-success'>Add +</button>
+                <Link to="/create" className='btn btn-success'>Add +</Link>
                 <table className='table'>
                     <thead>
                         <tr>
