7b172ea84afff1ce964ccdf0d462ed662c7ec3eb
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-28 15:32:30 +0000

    revterted changed with db to test ouput

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Employee from './Employee.js';
+import Employee from './Employee';
 
 
 function App() {
--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -3,31 +3,9 @@ import axios from 'axios'
 import useEffect from 'react'
 
 function Employee() {
-
-    useEffect(()=> {
-        axios.get('http://172.31.4.53:8081/')
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-    },[])   
-
-    return (
-        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-            <div className='w-50 bg-white rounded'>
-                <button className='btn btn-success'>Add +</button>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+   return (
+    <div>Employee</div>
+   )
 }
 
 export default Employee
