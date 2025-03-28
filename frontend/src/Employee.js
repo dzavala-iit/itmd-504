303415198000ca1b0584abdb9e124e91eb4cbc9c
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-28 15:33:57 +0000

    output worked, readded the db code

--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -3,8 +3,29 @@ import axios from 'axios'
 import useEffect from 'react'
 
 function Employee() {
-   return (
-    <div>Employee</div>
+    useEffect(()=> {
+        axios.get('http://172.31.4.53:8081/')
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    },[])   
+
+    return (
+        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+            <div className='w-50 bg-white rounded'>
+                <button className='btn btn-success'>Add +</button>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
    )
 }
 
