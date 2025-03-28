c95eec28da1c908c2acbdf6aecc80b05b9e62ed5
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-28 16:14:24 +0000

    re-addedd the useEffect code

--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
-import useEffect from 'react'
+
 
 function Employee() {
     
-
+    useEffect(() => {
+        axios.get('http://localhost:8081/')
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    })
     return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-5'>
