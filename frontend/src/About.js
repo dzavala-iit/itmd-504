a23ec1ec5976a45c09cbe17cc62fc17b97aa632b
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-31 20:27:14 +0000

    adjusted about page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -7,7 +7,7 @@ function About() {
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-5'>
                 <h2>About</h2>
-                <p>This is a simple employee management system built with Java, React, Node and Express.</p>
+                <p>This is a simple employee management system built with bootstrap, Java, React, Node and Express.</p>
                 <p>It allows you to add, update, and delete employees from the database.</p>
                 <p>It uses MySQL as the database.</p>
                 <p>It was created by Damian Zavala for the final project of ITMD 504</p>
