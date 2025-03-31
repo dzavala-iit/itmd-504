d956df97e326ba00f4fc69cbac74faf0d436a503
dzavala-iit <dzavala@hawk.iit.edu>
2025-03-31 19:56:40 +0000

    added name ot table

--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -21,7 +21,9 @@ function Employee() {
     };
     return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+            
             <div className='w-50 bg-white rounded p-5'>
+                <h2>Employee Lookup Datbase</h2>
                 <Link to="/create" className='btn btn-success'>Add +</Link>
                 <table className='table'>
                     <thead>
