327ae183f5c5c2b3e6b7d99d83da18cb9439ee1a
dzavala-iit <dzavala@hawk.iit.edu>
2025-04-10 19:23:51 +0000

    updating  backend ip for database

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "172.31.4.53",
     user: "root",
     password: "",
     database: "itmd504"
